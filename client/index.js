9cb018faa3bcd518c0b626949771b18e40fe3222
Yvette Chica <yvette@its6am.com>
2018-02-04 01:28:46 -0500

    Set up react

---
+++ client/index.js
@@ -0,0 +1,8 @@
+/*
+    This is the webpack entry file
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
